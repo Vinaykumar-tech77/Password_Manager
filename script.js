a904b4f2956a557630666bf586c5e74fb556170b
Vinaykumar-tech77 <127099413+Vinaykumar-tech77@users.noreply.github.com>
2025-06-23 00:25:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,106 @@
+function togglePasswordInput() {
+    const checkbox = document.getElementById("useGenerated");
+    const passwordInput = document.getElementById("password");
+    const generateBtn = document.getElementById("generateBtn");
+
+    if (checkbox.checked) {
+        passwordInput.disabled = true;
+        passwordInput.value = ""; // clear manual input
+        generateBtn.style.display = "inline-block";
+    } else {
+        passwordInput.disabled = false;
+        generateBtn.style.display = "none";
+    }
+}
+
+
+function generatePassword(length = 8) {
+    const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+[]{};:,.<>?";
+    let password = "";
+    for (let i = 0; i < length; i++) {
+        const randIndex = Math.floor(Math.random() * chars.length);
+        password += chars[randIndex];
+    }
+
+    const passwordInput = document.getElementById("password");
+    passwordInput.value = password;
+
+    navigator.clipboard.writeText(password).then(() => {
+        alert("Generated password copied to clipboard!");
+    });
+}
+
+
+
+
+
+function maskPassword(pass){
+    let str = ""
+    for(let index = 0;index < pass.length; index++){
+        str += "*"
+    }
+    return str
+}
+function copyText(txt){
+    navigator.clipboard.writeText(txt).then(
+        () => {
+            //alert("Copied the Text:"+txt);
+            const alertEl = document.querySelector(".alert");
+                alertEl.classList.remove("alert");
+                setTimeout(() => {
+                    alertEl.style.display = "none";
+                }, 2000);
+        },
+        () => {
+            alert("Copying Failed!!!")
+        },
+    );
+}
+const deletePassword = (platform)=>{
+    let data = localStorage.getItem("passwords")
+    let arr = JSON.parse(data);
+    arrUpdated = arr.filter((e)=>{
+        return e.platform != platform
+    })
+    localStorage.setItem("passwords",JSON.stringify(arrUpdated))
+    alert(`Sucessfully Deleted ${platform}'s Password!!!`)
+    showPasswords()
+}
+showPasswords = ()=>{
 let tb = document.querySelector("table")
 let data = localStorage.getItem("passwords")
-if(data == null){
+if(data == null || JSON.parse(data).length == 0){ 
     tb.innerHTML = "No Data Available!!!"
 }
 else{
+    tb.innerHTML = `<tr>
+                    <th>Platform</th>
+                    <th>Username</th>
+                    <th>Password</th>
+                    <th>Delete</th>
+                </tr>`
     let arr = JSON.parse(data);
     let str = ""
-    for(let index = 0;index<arr.length;i++){
+    for(let index = 0;index<arr.length;index++){
         const element = arr[index];
-        str= `<tr>
-        <td>${element.platform}</td>
-        <td>${element.username}</td>
-        <td>${element.password}</td>
-        <td>${"delete"}</td>
+        str += `<tr>
+        <td>${element.platform}<img onclick="copyText('${element.platform}')" src="copy.svg" alt="Copy Icon" />
+</td>
+        <td>${element.username}<img onclick="copyText('${element.username}')" src="copy.svg" alt="Copy Icon"  />
+</td>
+        <td>${maskPassword(element.password)}<img onclick="copyText('${element.password}')" src="copy.svg" alt="Copy Icon" />
+</td>
+        <td><button class="btnn" onclick="deletePassword('${element.platform}')">Delete</button></td>
         </tr>`
     }
     tb.innerHTML = tb.innerHTML + str
+}   
+platform.value = ""
+username.value = ""
+password.value = "" 
 }
 console.log("Working");
+showPasswords()
 document.querySelector(".btn").addEventListener("click", (e)=>{
     e.preventDefault()
     console.log("Clicked");
@@ -26,14 +109,15 @@ document.querySelector(".btn").addEventListener("click", (e)=>{
     console.log(passwords)
     if(passwords == null){
         let json = []
-        json.push({username:username.value,password:password.value})
+        json.push({platform:platform.value,username:username.value,password:password.value})
         alert("Credentials Saved!!!")
         localStorage.setItem("passwords",JSON.stringify(json))
     }
     else{
         let json = JSON.parse(localStorage.getItem("passwords"))
-        json.push({username:username.value,password:password.value})
+        json.push({platform:platform.value,username:username.value,password:password.value})
         alert("Cerdentials Saved!!!")
         localStorage.setItem("passwords",JSON.stringify(json))
     }
+    showPasswords()
 })
